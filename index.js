1e7498a56aaf4b55006526294bc4da534d52dcd2
hieule2k <91259565+hieule2k@users.noreply.github.com>
2021-11-24 13:46:27 +0700

    update

--- index.js
+++ index.js
@@ -53,60 +53,60 @@ for (let i = 0; i < placeList.length; i++) {
     document.getElementsByClassName("place-list")[0].appendChild(div);
 }
 
-var btn = document.createElement("Button");
-document.getElementsByClassName("content-section")[1].appendChild(btn);
-btn.setAttribute("class", "event");
-document.querySelector(".event").innerHTML = "try me";
 
-function createDiv() {
-    var placeAdd = { name: "Ha Noi", time: "Fri 27 Nov 2016", desc: "Praesent tincidunt sed tellus ut rutrum sed vitae justo.", tickets: "Buy Tickets" };
+function addNewItem(){
 
-    var nameTextNode1 = document.createTextNode(placeAdd.name);
-    var timeTextNode1 = document.createTextNode(placeAdd.time);
-    var descTextNode1 = document.createTextNode(placeAdd.desc);
-    var ticketsTextNode1 = document.createTextNode(placeAdd.tickets);
-    var headAdd = document.createElement("H3");
-    var paraAdd = document.createElement("p");
-    var para2Add = document.createElement("P");
-    var aadd = document.createElement("A");
+    let inputHeadAdd = document.getElementsByClassName("input-add")[0].value,
+        inputHeadTextNode = document.createTextNode(inputHeadAdd),
+        inputDateAdd = document.getElementsByClassName("input-add")[1].value,
+        inputDateTextNode = document.createTextNode(inputDateAdd),
+        inputDescAdd = document.getElementsByClassName("input-add")[2].value,
+        inputDescTextNode = document.createTextNode(inputDescAdd),
+        header = document.createElement("h2"),
+        date = document.createElement("P"),
+        desc = document.createElement("P");
+        aadd = document.createElement("A");
 
+    header.appendChild(inputHeadTextNode);
+    date.appendChild(inputDateTextNode);
+    desc.appendChild(inputDescTextNode);
+
+    var divAdd = document.createElement("DIV");
     var imgAdd = document.createElement("IMG");
     var divPlaceBodyAdd = document.createElement("DIV");
-    var divAdd = document.createElement("DIV");
 
-    headAdd.appendChild(nameTextNode1);
-    paraAdd.appendChild(timeTextNode1);
-    para2Add.appendChild(descTextNode1);
-    aadd.appendChild(ticketsTextNode1);
-
-    divPlaceBodyAdd.appendChild(headAdd);
-    divPlaceBodyAdd.appendChild(paraAdd);
-    divPlaceBodyAdd.appendChild(para2Add);
-    divPlaceBodyAdd.appendChild(aadd);
-
-    divAdd.appendChild(imgAdd);
-    divAdd.appendChild(divPlaceBodyAdd)
-
-    divAdd.setAttribute("class", "col col-third mt-16 s-col-full")
-    imgAdd.setAttribute("src", "./assets/img/place/place 1.jpg");
     imgAdd.setAttribute("class", "place-img");
-    imgAdd.setAttribute("alt", "New York");
+    imgAdd.setAttribute("src", "./assets/img/place/place 1.jpg");
+    imgAdd.setAttribute("alt", "");
+    divAdd.setAttribute("class", "col col-third mt-16 s-col-full");
     divPlaceBodyAdd.setAttribute("class", "place-body");
-    headAdd.setAttribute("class", "place-heading");
-    paraAdd.setAttribute("class", "place-time");
-    para2Add.setAttribute("class", "place-desc");
-    aadd.setAttribute("src", "");
+    header.setAttribute("class", "place-heading");
+    date.setAttribute("class", "place-time");
+    desc.setAttribute("class", "place-desc");
     aadd.setAttribute("class", "btn s-full-width");
-    return divAdd;
+
+    
+    divAdd.appendChild(imgAdd);
+    divPlaceBodyAdd.appendChild(header);
+    divPlaceBodyAdd.appendChild(date);
+    divPlaceBodyAdd.appendChild(desc);
+    divPlaceBodyAdd.appendChild(aadd);
+    divAdd.appendChild(divPlaceBodyAdd);
+    
+    var textInButton = document.createTextNode("Buy Tickets");
+    aadd.appendChild(textInButton);
+
+
+
+    return(divAdd);
 }
 
+var y = document.querySelector(".add-button");
+y.addEventListener('click', function(){
+    document.getElementsByClassName("place-list")[0].appendChild(addNewItem());
 
-
-
-var y = document.querySelector(".event")
-y.addEventListener("click", function(){
-    document.getElementsByClassName("place-list")[0].appendChild(createDiv());
 })
+
 
 
 
